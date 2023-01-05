811bc868d1df0557d69eb13a6364e627accf93b6
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2023-01-05 12:50:27 -0800

    bugs fxied

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 
-//functionalities start
-window.onload = function() {
-    const input = document.getElementById("password-input");
+//     // const outerDiv = document.getElementById("out");
+//     // const passwordDiv = document.createElement("Div");
+//     // passwordDiv.classList.add("under");
+//     // outerDiv.append(passwordDiv)
 
-    const button = document.getElementById("password-button");
+// //functionalities start
+//     let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     let passwordLength = 8;
+//     let generatedPassword = ""
+//     const input = document.getElementById("password-input");
 
-    const outerDiv = document.getElementById("out");
-    const passwordDiv = document.createElement("Div");
-    passwordDiv.classList.add("under");
-    outerDiv.append(passwordDiv)
+//     const button = document.getElementById("password-button");
+//     for(let i = 0; i<= passwordLength; i++) {
+//         const new1 = Math.floor(Math.random() * chars.length)
+//     }
 
+//     button.addEventListener("click", ()=> {
+       
+//     })
 
-    button.addEventListener("click", ()=> {
-        if(input.value == 0) {
-            passwordDiv.style.visibility = "hidden"
-        }
-       passwordDiv.innerHTML = `
-       <li>
-         ${input.value}
-       </li>
-       ` 
+//     input.value = new1;
 
-       //clearing the input element after click
-       input.value = "";
-    })
-}
+//     function generate() {
+        
+//     }
+var password=document.getElementById("password-input");
 
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password-input").value = password;
+ }
 //functionalities end
