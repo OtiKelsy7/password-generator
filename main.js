2af4fd1cfe53ee4a2a94d465d92b56223a5b7bd1
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2023-01-09 15:02:35 -0800

    upgraded but have bugs to fix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //functionalities start
 var password=document.getElementById("password-input");
+let outter = document.getElementById("out")
 
  function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,5 +11,17 @@ var password=document.getElementById("password-input");
    password += chars.substring(randomNumber, randomNumber +1);
   }
         document.getElementById("password-input").value = password;
+ }
+
+ function copyPassword() {
+   var copyText = document.getElementById("password-input");
+   copyText.select();
+   document.execCommand("copy");
+   let copied = document.createElement("div")
+   copied.classList.add("popup")
+   copied.innerText = "copied"
+   outter.append(copied)
+   setTimeout(copied, 1000);
+   clearTimeout(copied)
  }
 //functionalities end
